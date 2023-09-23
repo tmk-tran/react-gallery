8397139f8688c75b2098d0b130cf29cde2019a5a
T Mark <tmk1.tran@gmail.com>
2023-09-23 16:13:17 -0500

    added images to folder, updated Gallery Items

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,14 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, title: 'Kaeden', path: 'images/IMG_2CE.jpeg', description: 'Photo of lil Kaeden as a baby', likes: 0 },
+    { id: 3, title: '?', path: 'images/IMG_8304.jpeg', description: 'Photo of ?', likes: 0 },
+    { id: 4, title: 'Change Me', path: 'images/IMG_8360.jpeg', description: 'Photo of change me', likes: 0 },
+    { id: 5, title: 'Change Me2', path: 'images/IMG_8442.jpg', description: 'Photo of chenge me 2', likes: 0 }
 ];
 
 module.exports = galleryItems;
+
+
+
+// modify the `server/modules/data.js` to include an `id`, `title`, `description` and `path for` each of your images. 
+// Each `id` should be a unique number (e.g. 1, 2, 3...).
