9e29e4d455fa702793525f8f22a05a965ac03c4d
T Mark <tmk1.tran@gmail.com>
2023-09-23 22:04:05 -0500

    moved data to DB. set up GET route in router. installed pg, and created pool.js

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+const galleryItems = require("../modules/gallery.data");
+const pool = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
+router.put("/like/:id", (req, res) => {
+  console.log('like clicked for: ',req.params);
+  const galleryId = req.params.id;
+  for (const galleryItem of galleryItems) {
+    if (galleryItem.id == galleryId) {
+      galleryItem.likes += 1;
     }
-    res.sendStatus(200);
+  }
+  res.sendStatus(200);
 }); // END PUT Route
 
-// GET Route
-router.get('/', (req, res) => {
-    res.send(galleryItems);
-}); // END GET Route
+// GET Route- original
+// router.get("/", (req, res) => {
+//   res.send(galleryItems);
+// });
+
+// GET route for database (stretch goal)
+router.get("/", (req, res) => {
+  const queryText = `SELECT * FROM "gallery";`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error making database GET request`, error);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
