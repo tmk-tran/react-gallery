b140641e4919b05c00f74439da6ddedc6d4b9631
T Mark <tmk1.tran@gmail.com>
2023-09-24 23:03:57 -0500

    added multer again, description saves, but issue with upload of image. need to address later

--- server/server.js
+++ server/server.js
@@ -1,17 +1,68 @@
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const gallery = require('./routes/gallery.router.js');
+// const PORT = process.env.PORT || 5000;
+
+// /** ---------- MIDDLEWARE ---------- **/
+// app.use(bodyParser.json()); // needed for axios requests
+// app.use(express.static('build'));
+
+// /** ---------- EXPRESS ROUTES ---------- **/
+// app.use('/gallery', gallery);
+
+// /** ---------- START SERVER ---------- **/
+// app.listen(PORT,  () => {
+//     console.log('Listening on port: ', PORT);
+// });
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const multer = require('multer'); // Import Multer
+const path = require('path'); // Import the 'path' module
 const gallery = require('./routes/gallery.router.js');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
+
 app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
 
+/** ---------- MULTER CONFIGURATION ---------- **/
+
+// Set up storage for uploaded images
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Store uploaded files in the 'uploads' directory
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix =
+      Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname)); // Generate a unique filename
+  },
+});
+
+// Create a Multer instance with the storage configuration
+const upload = multer({ storage: storage });
+
 /** ---------- EXPRESS ROUTES ---------- **/
+
+// Define a route for image uploads
+app.post('/upload', upload.single('image'), (req, res) => {
+  // The uploaded file is available in req.file
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+  const imagePath = req.file.path;
+  // Handle saving the imagePath to your database or wherever you need it
+  res.status(200).send('Image uploaded successfully!');
+});
+
 app.use('/gallery', gallery);
 
 /** ---------- START SERVER ---------- **/
-app.listen(PORT,  () => {
-    console.log('Listening on port: ', PORT);
+
+app.listen(PORT, () => {
+  console.log('Listening on port: ', PORT);
 });
--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -1,66 +1,154 @@
+// import React, { useState } from "react";
+// // Import the TextField component from Material-UI
+// import TextField from "@mui/material/TextField";
+// import ClearIcon from "@mui/icons-material/Clear";
+// import Button from '@mui/material/Button';
+// import AddIcon from '@mui/icons-material/Add';
+
+// export default function GalleryForm({ addImage }) {
+//   const [showForm, setShowForm] = useState(false);
+//   const [newUrl, setNewUrl] = useState("");
+//   const [description, setDescription] = useState("");
+
+//   // function to toggle the form
+//   const toggleForm = () => {
+//     setShowForm(!showForm);
+//     console.log(showForm);
+//     setNewUrl("");
+//   };
+
+//   //function for handleAdd
+//   const handleAdd = () => {
+//     if(newUrl === '' || undefined) {
+//         alert('Please enter an image url!');
+//     } else {
+//     addImage(newUrl, description);
+//     setNewUrl("");
+//     setDescription("");
+//     toggleForm();
+//   }};
+
+//   return (
+//     <div>
+//       {!showForm ? (
+//         <div className="toggleBtn">
+//           <div className="add-container">
+//             <Button className="addBtn" onClick={toggleForm} variant="contained" color="secondary">
+//                 <AddIcon />
+//             </Button>
+//           </div>
+//         </div>
+//       ) : (
+//         <div className="add-form">
+//             <ClearIcon
+//               className="close-button"
+//               onClick={toggleForm}
+//             />
+//           <TextField
+//             variant="outlined"
+//             label="Enter Image URL"
+//             value={newUrl}
+//             onChange={(event) => setNewUrl(event.target.value)}
+//             required
+//           />  
+//           <TextField
+//             className="description"
+//             variant="filled"
+//             label="Enter Description (optional)"
+//             value={description}
+//             onChange={(event) => setDescription(event.target.value)}
+//           />
+//           <button className="add-image" onClick={handleAdd}>
+//             ADD
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+
 import React, { useState } from "react";
-// Import the TextField component from Material-UI
 import TextField from "@mui/material/TextField";
-import ClearIcon from "@mui/icons-material/Clear";
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function GalleryForm({ addImage }) {
   const [showForm, setShowForm] = useState(false);
   const [newUrl, setNewUrl] = useState("");
   const [description, setDescription] = useState("");
+  const [imageFile, setImageFile] = useState(null); // State to store the selected image file
 
   // function to toggle the form
   const toggleForm = () => {
     setShowForm(!showForm);
     console.log(showForm);
     setNewUrl("");
+    setImageFile(null); // Clear selected image file when toggling form
   };
 
-  //function for handleAdd
+  // function for handleAdd
   const handleAdd = () => {
-    if(newUrl === '' || undefined) {
-        alert('Please enter an image url!');
+    if (!imageFile) {
+      alert("Please select an image to upload!");
     } else {
-    addImage(newUrl, description);
-    setNewUrl("");
-    setDescription("");
-    toggleForm();
-  }};
+      // Call the addImage function with the selected image file and description
+      addImage(imageFile, description);
+      setNewUrl("");
+      setDescription("");
+      toggleForm();
+    }
+  };
+
+  // Handle image file selection
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImageFile(file);
+  };
 
   return (
     <div>
       {!showForm ? (
         <div className="toggleBtn">
           <div className="add-container">
-            <Button className="addBtn" onClick={toggleForm} variant="contained" color="secondary">
-                <AddIcon />
+            <Button
+              className="addBtn"
+              onClick={toggleForm}
+              variant="contained"
+              color="secondary"
+            >
+              <AddIcon />
             </Button>
           </div>
         </div>
       ) : (
         <div className="add-form">
-            <ClearIcon
-              className="close-button"
-              onClick={toggleForm}
-            />
+          <Button
+            onClick={toggleForm}
+            variant="outlined"
+            color="secondary"
+          >
+            Cancel
+          </Button>
           <TextField
             variant="outlined"
-            label="Enter Image URL"
-            value={newUrl}
-            onChange={(event) => setNewUrl(event.target.value)}
-            required
-          />  
-          <TextField
-            className="description"
-            variant="filled"
-            label="Enter Description (optional)"
+            label="Enter Image Description (optional)"
             value={description}
             onChange={(event) => setDescription(event.target.value)}
           />
-          <button className="add-image" onClick={handleAdd}>
-            ADD
-          </button>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange} // Handle image file selection
+          />
+          <Button
+            className="add-image"
+            onClick={handleAdd}
+            variant="contained"
+            color="primary"
+          >
+            Upload
+          </Button>
         </div>
       )}
     </div>
